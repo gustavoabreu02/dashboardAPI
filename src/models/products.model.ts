163670490124f044f2253075a5e398b10e863120
Gustavo Abreu <gustavofart371@gmail.com>
2023-05-06 22:59:57 -0300

    06-05

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -1,16 +1,16 @@
-import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import { /* ResultSetHeader, */ RowDataPacket } from 'mysql2';
 import { IProduct } from '../interfaces';
 import connection from './connection';
 
 export default class ProductsModel {
   connection = connection;
 
-  async getAllProducts(): Promise<IProduct[]> {
+  /*   async getAllProducts(): Promise<IProduct[]> {
     const [rows] = await this.connection.execute<IProduct[] & RowDataPacket[]>(
       'SELECT * FROM Trybesmith.Products;',
     );
     return rows;
-  }
+  } */
 
   async getProductCodigo(product: IProduct): Promise<IProduct[]> {
     const sql = `SELECT PRODUTO FROM DashboardAPI.Vendas WHERE CODPROD = (?)
@@ -22,8 +22,8 @@ export default class ProductsModel {
     return result;
   }
 
-  async updateProduct(productId: number, insertId: number) {
+/*   async updateProduct(productId: number, insertId: number) {
     const sql = 'UPDATE Trybesmith.Products SET orderId = ? WHERE id = ?';
     await this.connection.execute<ResultSetHeader>(sql, [insertId, productId]);
-  }
+  } */
 }
